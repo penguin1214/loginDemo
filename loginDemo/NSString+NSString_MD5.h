394609850f8c8b031312828dfe01992b81b80fcd
penguin-penpen <penguingohack@outlook.com>
2016-05-13 20:48:45 +0800

    add md5 processing category

---
+++ loginDemo/NSString+NSString_MD5.h
@@ -0,0 +1,15 @@
+//
+//  NSString+NSString_MD5.h
+//  loginDemo
+//
+//  Created by 杨京蕾 on 5/13/16.
+//  Copyright © 2016 yang. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NSString (NSString_MD5)
+
+- (NSString*)MD5String;
+
+@end
